7ad40d70e22ce8daa41a2113f16520711362e05b
Cesar Gomez <zootechdrum@gmail.com>
2020-09-18 20:31:10 -0700

    added dropdown component from react-bootstrap also added backend tech

---
+++ client/src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Hero from "../components/Hero";
+import { DropdownButton, Dropdown } from "react-bootstrap";
+import Container from "../components/Container";
+import "./Home.css";
+
+import FenderLogo from "../images/Fender_logo.png";
+
+function Home() {
+  return (
+    <div>
+      <Hero>
+        <img src={FenderLogo} alt="Fender Logo" />
+      </Hero>
+      <Container style={{ marginTop: 50 }}>
+        <div className="text-center">
+          <DropdownButton id="dropdown-button" title="Filter by Component">
+            <Dropdown.Item href="#/action-1">Electronics</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Finish</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Body</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Hardware</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Neck</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Nut</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Acoustic</Dropdown.Item>
+          </DropdownButton>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
---
+++ scripts/seedDB.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/defects", {
+  useNewUrlParser: true,
+});
+
+const errorSeed = [
+  {
+    errorCode: 2000,
+    title: "Pickup Out of Phase",
+    Description:
+      "When they are out-of-phase, the two pickups work against one another; the resulting sound is simply the “leftovers” from the pickups’ cancellations. The closer the two pickups are, the greater the cancellations, meaning thinner sound and lesser volume.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+];
+
+db.Defects.remove({})
+  .then(() => db.Post.collection.insertMany(errorSeed))
+  .then((data) => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });

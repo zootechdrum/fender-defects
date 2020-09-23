10b25dd1dbc9d9bf1b54b29f5905444e9be385e3
Cesar Gomez <zootechdrum@gmail.com>
2020-09-22 22:19:27 -0700

    added last of page 1 defect codes also added repositioned arrow

---
+++ scripts/body-neckDefects.js
@@ -0,0 +1,241 @@
+const bodyAndNeckDefects = [
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: ,
+    title: "",
+    component: "",
+    description:
+      "",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  
+];
+
+module.exports = {
+  bodyAndNeckDefects: bodyAndNeckDefects,
+};
---
+++ scripts/nutDecects.js
@@ -0,0 +1,84 @@
+const nutDefects = [
+  {
+    errorCode: 2440,
+    title: "Profiling Issue",
+    component: "Nut",
+    description:
+      "This is when there is a problem with the shape, size, or finished look of the nut or if the nut is damaged.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2441,
+    title: "Chipped",
+    component: "Nut",
+    description:
+      "This is when a portion of the nut material has broken out leaving the nut damaged. Note: This usually occurs during the nut slotting process.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2450,
+    title: "Slots High/Low",
+    component: "Nut",
+    description:
+      "Slots are high or low when they do not meet height specifications. Note: High nut slots ",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2451,
+    title: "Shelf High/Low (2451)",
+    component: "Nut",
+    description:
+      "When the shelf is machined too high or low, the nut cannot be cut within specification. Note: The result of the shelf being too high or low is too deep or too shallow string slots.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2452,
+    title: "Spacing Issue",
+    component: "Nut",
+    description:
+      "Spacing issues occur when the space between strings are too wide or too narrow per specification.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2453,
+    title: "Not Seated",
+    component: "Nut",
+    description:
+      "A nut is not seated when the bottom of the nut fails to make full contact with the nut shelf.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+];
+
+module.exports = {
+  nutDefects: nutDefects,
+};

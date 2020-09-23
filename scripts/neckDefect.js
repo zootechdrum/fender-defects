0ddfa256e04ff568bd213d0dd0643fd37dd9f15f
Cesar Gomez <zootechdrum@gmail.com>
2020-09-22 20:43:42 -0700

    finished neck defect file

---
+++ scripts/neckDefect.js
@@ -0,0 +1,305 @@
+const neckDefects = [
+  {
+    errorCode: 2350,
+    title: "Stripped Screw",
+    component: "Neck",
+    description:
+      "A decal issue is a problem with the logo or decal. Note: Examples include damage, misalignment, air bubbles, clarity, placement, correct logo, and raised or lifting.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2351,
+    title: "Binding Issue",
+    component: "Neck",
+    description:
+      "A binding issue is an inconsistency with the binding in color, thickness, height or where damage has occurred.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2352,
+    title: "Binding Issue",
+    component: "Neck",
+    description:
+      "A face dot issue can be damage, incorrect placement, inconsistency in shape and color, poor workmanship of installation and incorrect inlay or face dot for the specific model.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2353,
+    title: "Side Dot Issue",
+    component: "Neck",
+    description:
+      "Side dot issues can be damage, incorrect placement, inconsistency in shape and color, poor workmanship of installation and the use of incorrect material for the specific model.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2354,
+    title: "String Tree Alignment",
+    component: "Neck",
+    description:
+      "String tree alignment is when the placement of the string tree causes the strings to pull to one side after leaving the string nut.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2355,
+    title: "Mineral Stain (out of criteria)",
+    component: "Neck",
+    description:
+      "An out-of-criteria mineral stain is a discoloration that exceeds the criteria for an allowable mineral stain in size, length or placement. They are seen as olive to greenish-black or brown, or sometimes even purple.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2356,
+    title: "Wood Cracked",
+    component: "Neck",
+    description:
+      "A crack in the wood is any separation of natural wood fibers as opposed to wood that has been joined by glues or pressed together and separates at the seam (known as a seam line).",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2360,
+    title: "Open Skunk Stripe",
+    component: "Neck",
+    description:
+      "This is a gap or separation between the walls of the neck and the truss rod filler strip (skunk stripe). This includes the top and bottom end of the filler strip.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2365,
+    title: "Tuning Key Alignment",
+    component: "Neck",
+    description:
+      " Tuning key alignment is when tuning keys are not mounted in a uniform manner.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2370,
+    title: "Pitch/Tilt Incorrect",
+    component: "Neck",
+    description:
+      "Incorrect pitch or tilt is when the relationship of the angle of the neck to the body is incorrect. This defect is also known as a neck angle problem. Note: This is usually first noticed when there are action adjustment problems because the string action cannot be controlled within the range of saddle or bridge adjustments within specification. Other issues that may be encountered with this problem are incorrect string pressure over the saddle on tremolo equipped guitars and gaps between the neck and body on bolt on type instruments. String breakage is another problem on instruments that have too high of a positive neck pitch.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2371,
+    title: "Pocket High or Low",
+    component: "Neck",
+    description:
+      "The pocket is high or low when the area of the body where the neck is to be attached is incorrectly cut. Note: One indicator of this is that the hardware will exceed its adjustment limits.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2380,
+    title: "Truss Rod/Neck Bow Issue",
+    component: "Neck",
+    description:
+      "Truss rod or neck bow issues occur when the truss rod functions but does not have sufficient control or travel to adjust the neck correctly. This defect can also be a neck that has irregularities such as humps or an unnatural amount of twist that cannot be controlled by adjusting the truss rod.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2390,
+    title: "Truss Rod/Mechanical Failure",
+    component: "Neck",
+    description:
+      "Truss rod/mechanical failure is when the truss rod fails to function properly. When trying to make adjustments, no change to the neck occurs when the adjustment nut is turned or the truss rod is frozen and cannot be adjusted.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2391,
+    title: "Truss Rod Dowel Loose ",
+    component: "Neck",
+    description:
+      "This is when the truss rod dowel plug becomes loose. Note: The plug is near the nut where the truss rod adjustment access point is located on some instruments.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2392,
+    title: "Truss Rod Off Center",
+    component: "Neck",
+    description:
+      "This is when the truss rod is not equidistant, per specification, from each side of the neck.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2410,
+    title: "Fret High/Low",
+    component: "Neck",
+    description:
+      "This is when the crown of a fret or multiple frets are not at the same height from the fretboard as the others. Note: This can cause a buzzing or choking out of the note when played in a specific area.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2411,
+    title: "Lifted Frets",
+    component: "Neck",
+    description:
+      "Frets that have gap between the top of the fingerboard and bottom of the fret wire is considered to be lifted. Note: A feeler gauge of .004” is used to check for lifted frets.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2420,
+    title: "Fret Flat/Sharp/Damage",
+    component: "Neck",
+    description:
+      "A sharp fret is a protrusion felt along the edge of the fingerboard. A flat fret has little or no crown and the top of the fret wire is wider then 1/32”. A damaged fret may contain dents, dings, grooves, scratches or any other anomalies that make the fret unusable.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2431,
+    title: "Alignment",
+    component: "Neck",
+    description:
+      "This is when the centerline of the neck, bridge and body are not in alignment. Note: For this specific defect, the neck is out of alignment with the body and bridge.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2432,
+    title: "Over-sanded",
+    component: "Neck",
+    description:
+      "An over-sanded neck has a misshapen area that has been sanded out of specification.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2433,
+    title: "Too Thin",
+    component: "Neck",
+    description:
+      "A neck is considered too thin when measurements taken of the neck are out of tolerance per specification.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2430,
+    title: "Uneven Butt(wedged)",
+    component: "Neck",
+    description:
+      "An uneven butt is when the heel is out of tolerance for flatness or has uneven sides due to machining or over-sanding. Either of these issues can cause the neck to be wedged into the pocket.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+  {
+    errorCode: 2430,
+    title: "Twisted/Warped",
+    component: "Neck",
+    description:
+      "A twisted neck is when the neck has a side to side irregularity as in a neck that may be under-bowed on one side and over-bowed on the other side. A warped neck is a neck that displays horizontal irregularities such as humps, dips, or bends from the nut to the base of the neck.",
+    media: {
+      videoUrl:
+        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
+      blog:
+        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
+    },
+  },
+];
+
+module.exports = {
+  neckDefects: neckDefects,
+};

0ce8f255660a4f3fe04b390fcc8cc6bdcf73af11
Cesar Gomez <zootechdrum@gmail.com>
2020-09-23 11:04:11 -0700

    added more defects for body.js file

--- scripts/bodyDefects.js
+++ scripts/bodyDefects.js
@@ -1,10 +1,10 @@
-const bodyAndNeckDefects = [
+const bodyDefects = [
   {
-    errorCode: ,
-    title: "",
-    component: "",
+    errorCode:2460 ,
+    title: "Neck/Body Fit – Gaps",
+    component: "Body",
     description:
-      "",
+      "This is when there is a problem with the joining of the body and neck. Issues can include excessive gaps, over-sanded and under-sanded surfaces, paint build up in critical areas and overall sloppy appearance to the fit.",
     media: {
       videoUrl:
         "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
@@ -13,11 +13,11 @@ const bodyAndNeckDefects = [
     },
   },
   {
-    errorCode: ,
-    title: "",
-    component: "",
+    errorCode:2461 ,
+    title: "Excessive Shelf",
+    component: "Body",
     description:
-      "",
+      "An excessive shelf is when the butt of the neck does not completely cover the neck pocket on the body leaving an excessive amount of the pocket exposed. The exposed flat area resembles a shelf.",
     media: {
       videoUrl:
         "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
@@ -26,11 +26,11 @@ const bodyAndNeckDefects = [
     },
   },
   {
-    errorCode: ,
-    title: "",
-    component: "",
+    errorCode:2480 ,
+    title: "Binding Issues",
+    component: "Body",
     description:
-      "",
+      "A binding issue is an inconsistency with the binding in color, thickness, height or where damage has occurred.",
     media: {
       videoUrl:
         "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
@@ -39,11 +39,11 @@ const bodyAndNeckDefects = [
     },
   },
   {
-    errorCode: ,
-    title: "",
-    component: "",
+    errorCode:2500 ,
+    title: "Over-sanded",
+    component: "Body",
     description:
-      "",
+      "This is a misshapen area that has been sanded out of specification.",
     media: {
       videoUrl:
         "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
@@ -52,11 +52,11 @@ const bodyAndNeckDefects = [
     },
   },
   {
-    errorCode: ,
-    title: "",
-    component: "",
+    errorCode:2510 ,
+    title: "Body – Too Heavy ",
+    component: "Body",
     description:
-      "",
+      "A body that is too heavy has a weight that is out of specification or does not meet standard criteria",
     media: {
       videoUrl:
         "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
@@ -65,11 +65,11 @@ const bodyAndNeckDefects = [
     },
   },
   {
-    errorCode: ,
-    title: "",
-    component: "",
+    errorCode:2515 ,
+    title: "Body – Too Thin",
+    component: "Body",
     description:
-      "",
+      "This is when a body measures too thin. Note: A body that is thinner than the specification may not work with other components or hardware.",
     media: {
       videoUrl:
         "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
@@ -78,11 +78,11 @@ const bodyAndNeckDefects = [
     },
   },
   {
-    errorCode: ,
-    title: "",
-    component: "",
+    errorCode: 3000 ,
+    title: "Neck/Body Fit – Joint Deformed at Body",
+    component: "Body",
     description:
-      "",
+    "This is when a body measures too thin. Note: A body that is thinner than the specification may not work with other components or hardware.",
     media: {
       videoUrl:
         "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
@@ -91,11 +91,11 @@ const bodyAndNeckDefects = [
     },
   },
   {
-    errorCode: ,
-    title: "",
-    component: "",
+    errorCode:3010 ,
+    title: "Neck/Body Fit ",
+    component: "Body",
     description:
-      "",
+      "A deformed joint at the neck is a separation at the joint due to an unstable neck.",
     media: {
       videoUrl:
         "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
@@ -104,11 +104,11 @@ const bodyAndNeckDefects = [
     },
   },
   {
-    errorCode: ,
-    title: "",
-    component: "",
+    errorCode: 3020,
+    title: "Kerfing/Lining Broken",
+    component: "Body",
     description:
-      "",
+      "Broken kerfing/lining are cracks, breaks or chips in the reinforcing strips that are glued onto the sides where the top and back meet the sides inside the body cavity.",
     media: {
       videoUrl:
         "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
@@ -117,11 +117,11 @@ const bodyAndNeckDefects = [
     },
   },
   {
-    errorCode: ,
-    title: "",
-    component: "",
+    errorCode: 3030 ,
+    title: "Bridge Delaminating/Lifting ",
+    component: "Body",
     description:
-      "",
+      "A bridge that is delaminating/lifting is pulling away from its glued surface on the top. Note: There is usually movement when the bridge is pressed or pushed.",
     media: {
       videoUrl:
         "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
@@ -130,11 +130,11 @@ const bodyAndNeckDefects = [
     },
   },
   {
-    errorCode: ,
-    title: "",
-    component: "",
+    errorCode: 3031,
+    title: "Bridge Cracked ",
+    component: "Body",
     description:
-      "",
+      "This is any type of break between wood fibers on the bridge.",
     media: {
       videoUrl:
         "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
@@ -237,5 +237,5 @@ const bodyAndNeckDefects = [
 ];
 
 module.exports = {
-  bodyAndNeckDefects: bodyAndNeckDefects,
+  bodyDefects: bodyDefects,
 };

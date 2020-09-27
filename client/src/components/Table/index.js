06c2a1446eae5df31fa07bd957115c8369ca26d9
Cesar Gomez <zootechdrum@gmail.com>
2020-09-27 10:18:33 -0700

    added image vector icon

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Table } from "react-bootstrap";
 import "./style.css";
 import youTubeImg from "../../images/youtube.png";
-import blogImg from "../../images/title.png";
+import blogImg from "../../images/img.png";
 
 function TableComp(props) {
   return (
@@ -28,13 +28,6 @@ function TableComp(props) {
 
             <td>
               <div className="d-flex align-items-center justify-content-around align-items-center">
-                <a href={defect.media.videoUrl}>
-                  <img
-                    className="tableIcon"
-                    src={youTubeImg}
-                    alt="Youtube Icon"
-                  />
-                </a>
                 <a href={defect.media.blogUrl}>
                   <img className="tableIcon" src={blogImg} alt="Youtube Icon" />
                 </a>

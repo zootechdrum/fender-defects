031129d1bd8b110d301ebdfcc6dc6539dfd4f6dd
Cesar Gomez <zootechdrum@gmail.com>
2020-09-29 19:23:38 -0700

    removed excess defect code

--- scripts/finishDefects.js
+++ scripts/finishDefects.js
@@ -359,19 +359,6 @@ const finishDefects = [
         "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
     },
   },
-  {
-    errorCode: 2310,
-    title: "Soft",
-    component: "Finish",
-    description:
-      "A soft finish is paint that has not thoroughly cured or dried. Note: Sometimes paint will take a few weeks to dry or can even remain permanently tacky. A soft finish will likely leave an impression on the surface with light pressure.",
-    media: {
-      videoUrl:
-        "https://www.youtube.com/watch?time_continue=6&v=xWq3v7DV7Zk&feature=emb_title",
-      blog:
-        "https://www.premierguitar.com/articles/phasing-out-how-to-get-out-of-phase-sounds-from-a-stratocaster-1",
-    },
-  },
 ];
 
 module.exports = {
